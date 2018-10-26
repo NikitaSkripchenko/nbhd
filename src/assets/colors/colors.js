4c0693961fb8adc266c381db92359a700decd78d
Nikita Skrypchenko <frikadelka456@gmail.com>
2018-10-26 23:49:29 +0300

    colors

--- src/assets/colors/colors.js
+++ src/assets/colors/colors.js
@@ -1,6 +1,7 @@
 const colors = {
-    orange: '#FF9100',
-    background: '#FFFBF6',
-
+    orange: "#FF9100",
+    background: "#FFFBF6",
+    green: "#00B103",
+    red: "#D0021B",
 };
 export default colors;
