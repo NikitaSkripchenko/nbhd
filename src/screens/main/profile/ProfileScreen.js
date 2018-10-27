76510efd5a29a70adb48d9e2a8f1a1e66d79fce5
Alexandr <sasha732sasha000@gmail.com>
2018-10-27 04:53:55 +0300

    profile a half, constants

--- src/screens/main/profile/ProfileScreen.js
+++ src/screens/main/profile/ProfileScreen.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 import { Text, View, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
-import { white } from "../../../assets/colors/colors";
+import colors from "../../../assets/colors/colors";
 
 const SettingsButton = ({ onPress, style }) => (
   <View style={style}>
     <Icon
       name={"settings"}
       size={30}
-      color={white}
+      color={colors.white}
       onPress={() => onPress()}
     />
   </View>
@@ -19,12 +19,16 @@ const SettingsButton = ({ onPress, style }) => (
 
 export default class ProfileScreen extends Component {
   static navigationOptions = ({ navigation }) => {
-    const { title, left, right } = appbarStyles;
+    const { title, right, header } = appbarStyles;
 
     return {
       title: "Profile",
       headerTitleStyle: title,
-      headerRight: (<SettingsButton onPress={() => navigation.navigate("")} style={right}/>)
+      headerStyle: header,
+      headerRight: (
+        <SettingsButton onPress={() => navigation.navigate("")} style={right} />
+      ),
+      headerLeft: <View />
     };
   };
 
@@ -33,15 +37,37 @@ export default class ProfileScreen extends Component {
   };
 
   render() {
-    const { header, history } = styles;
+    const {
+      headerContainer,
+      profileImageContainer,
+      userInfoContainer,
+      ratingContainer,
+      uNameCaption,
+      history
+    } = styles;
 
     return (
       <View>
-        <View style={header}>
-          <Text>Header here</Text>
+        <View style={headerContainer}>
+          <View style={profileImageContainer}>
+            <Text>I</Text>
+          </View>
+          <View style={userInfoContainer}>
+            <View style={uNameCaption}>
+              <Text style={styles.uName}>Hello Dude</Text>
+            </View>
+            <View style={ratingContainer}>
+              <Icon
+                name={"star"}
+                size={24}
+                color={colors.orange}
+              />
+              <Text>4.4</Text>
+            </View>
+          </View>
         </View>
         <View style={history}>
-          <Text>List of actyviry here</Text>
+          <Text>List of activity here</Text>
         </View>
       </View>
     );
@@ -49,13 +75,55 @@ export default class ProfileScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  header: {},
+  headerContainer: {
+    backgroundColor: colors.background,
+    display: "flex",
+    flexDirection: "row",
+    padding: 15
+  },
+  profileImageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "gray"
+  },
+  userInfoContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  uNameCaption: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  uName: {
+    fontSize: 17,
+    fontWeight: "500"
+  },
+  ratingContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center"
+  },
+
   history: {}
 });
 
-const appbarStyles = {
-  title: {},
-  right: { 
+const appbarStyles = StyleSheet.create({
+  header: {
+    backgroundColor: colors.orange
+  },
+  title: {
+    flex: 1,
+    textAlign: "center",
+    color: colors.white
+  },
+  right: {
     marginHorizontal: 14
   }
-};
+});
