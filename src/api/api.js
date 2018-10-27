764af842531f719e00dd44604a4e74b67637a2fe
Alexandr <sasha732sasha000@gmail.com>
2018-10-27 07:38:20 +0300

    redux for feed

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,19 @@
-const url = "http://192.168.163.56";
+const url = "http://192.168.43.244:8080";  // __main__
 
-export const getJSON = (endpoint, params) => {
+const getJSON = (endpoint, params) => {
   return fetch(`${url}/${endpoint}`, {
     method: "post",
     body: JSON.stringify(params)
   }).then(res => res.json());
 };
+
+export const callAction = ({type, apiEndpoint}, params) => {
+  return async(dispatch) => {
+    const json = await getJSON(apiEndpoint, params)
+
+    return dispatch({
+      type: type,
+      data: json
+    })
+  } 
+}
