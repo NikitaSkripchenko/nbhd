332f40a245834aa95a339b6cdaa8fe2362acf018
Nikita Skrypchenko <frikadelka456@gmail.com>
2018-10-27 09:51:55 +0300

    authNav

--- src/screens/auth/AuthNavigator.js
+++ src/screens/auth/AuthNavigator.js
@@ -2,17 +2,18 @@ import {createStackNavigator} from "react-navigation";
 import transitionConfig from "../../library/utils/transitionConfig";
 import AuthScreen from "./AuthScreen";
 import { AUTH_ROUTE } from "../../constants";
+import SignInScreen from "./SignInScreen";
 
 const AuthNavigator = createStackNavigator({
     AuthScreen: {screen: AuthScreen},
-        
+    SignInScreen: {screen: SignInScreen},
     },
     {
         headerMode: "float",
         transitionConfig,
         navigationOptions: {
             gesturesEnabled: true,
-            gestureResponseDistance: {horizontal: 200}
+            gestureResponseDistance: {horizontal: 600}
         },
         initialRouteName: AUTH_ROUTE,
     });
--- src/screens/auth/authScreen.js
+++ src/screens/auth/authScreen.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity  } from 'react-native';
+import {TextInput, } from 'react-native-paper';
+import colors from '../../assets/colors/colors';
 
 export default class AuthScreen extends Component {
   static navigationOptions ={
     header: null
+  };
+  state = {
+    phone: "",
+	name: "",
+	password: ""
+  }
+
+  goToSignIn(){
+	  const {navigate} = this.props.navigation;
+	  navigate("SignInScreen");
   }
   render() {
     return (
-      <View>
-        <Text> Login </Text>
+      <View style ={styles.container}>
+        <TextInput 
+            outlined
+            style = {{backgroundColor:"rgba(255,255,255,0)", marginHorizontal: 8}}
+            maxLenght = {13}
+			label = 'Phone'
+		    value={this.state.phone}
+            onChangeText={phone => this.setState({ phone })}/>
+		<TextInput 
+            outlined
+            style = {{backgroundColor:"rgba(255,255,255,0)",  marginHorizontal: 8}}
+            maxLenght = {16}
+			label = 'Password'
+		    value={this.state.password}
+            onChangeText={password => this.setState({ password })}/>
+        <TextInput 
+            outlined
+            style = {{backgroundColor: "rgba(255,255,255,0)",  marginHorizontal: 8}}
+            maxLenght = {43}
+            label = 'Name and Surname'
+            value={this.state.name}
+            onChangeText={name => this.setState({ name })}
+          />
+		<TouchableOpacity 
+			onPress = {()=>console.log(12)}
+			style = {styles.signUpButton}>
+			<Text style = {styles.text}> SIGN UP </Text>
+		</TouchableOpacity>
+		<View style = {{flexDirection: "row", position: "absolute", bottom: 10, alignSelf: "center", alignItems: 'center'}}>
+			<Text>Already have an account?</Text>
+			<TouchableOpacity 
+				onPress = {()=>this.goToSignIn()}
+				style = {{marginLeft: 8}}>
+			<Text style = {{fontSize: 17}}>SIGN IN</Text></TouchableOpacity>
+		</View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+
+    justifyContent: 'center',
+    backgroundColor: colors.background
+  },
+  signUpButton:{
+	backgroundColor: colors.orange,
+	borderRadius: 9,
+	height: 40,
+	margin: 16,
+	alignItems: 'center',
+	justifyContent: "center"
+  },
+  text:{
+	color:colors.white,
+	fontSize: 17,
+	fontWeight: "500"
+  },
+});
